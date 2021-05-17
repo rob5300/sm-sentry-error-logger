27e656f9a6dff2fc0eef39bbc65ed4f180d03bf6
John Chandara <email@john.science>
2021-05-17 17:14:46 -0500

    Build script refactored into parts
    Signed-off-by: John Chandara <email@john.science>

---
+++ .elf_build/1setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+_BasePath=$(git rev-parse --show-toplevel)
+_BaseDirName=$(git rev-parse --show-toplevel | grep -o --color=never '[A-Za-z_]*$')
+_ExtDirName=$(echo $_BasePathRoot | sed 's/ /_/g' | tr '[:upper:]' '[:lower:]') # 'CTF Repo' -> 'ctf_repo'
+#_ExtPath="./vendor/sourcemod/public/extensions/$_ExtDirName"
+
+# if [[ ! -d $_ExtPath ]]; then
+#     # Make it so SM can build our extension
+#     ln -sr "./" "$_ExtPath"
+# fi
+
+cd _BaseDirName
+./configure.py --hl2sdk-root ./vendor/source-sdk-2013 --mms-path ./vendor/metamod-source --sm-path ./vendor/sourcemod
