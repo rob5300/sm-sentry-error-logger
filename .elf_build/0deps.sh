ba757a80246353c1e7ac25e13d8d1aa91d4a88d6
John Chandara <email@john.science>
2021-05-20 15:09:18 -0500

    [bug] CI/CD missing deps

--- .elf_build/0deps.sh
+++ .elf_build/0deps.sh
@@ -6,7 +6,7 @@
 
 # apt-get install -y g++-4.1 gcc-4.1 make git bzip2
 
-apt-get install -y make git bzip2 python                                      # General dependencies
+apt-get install -y make git bzip2 python wget curl                            # General dependencies
 apt-get install -y libz libz-dev                                              # General dependencies
 apt-get install -y mono mono-devel python3 python3-pip                        # AMBuild tool
 apt-get install -y ia32-libs lib32z1 lib32z1-dev libc6-dev-i386 libc6-i386    # x86 support
