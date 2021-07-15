50e573dde36fc11550e2221250170d668c2b23a8
Robert <rob5300@hotmail.com>
2021-07-15 22:43:27 +0100

    Change server name default + try to use hostname as a backup.

--- src/CTFErrorLoggerConfig.cpp
+++ src/CTFErrorLoggerConfig.cpp
@@ -5,6 +5,16 @@ CTFErrorLoggerConfig::CTFErrorLoggerConfig(ICvar* icvar)
     server_id = icvar->FindVar("ce_server_index")->GetString();
     sentry_dsn_url = icvar->FindVar("ce_sentry_dsn_url")->GetString();
     server_name = icvar->FindVar("ce_server_name")->GetString();
+
+    if (server_name.length() < 1)
+    {
+        const ConVar* hostname = icvar->FindVar("hostname");
+        if (hostname != nullptr)
+        {
+            server_name = hostname->GetString();
+        }
+    }
+
     environment = icvar->FindVar("ce_environment")->GetString();
     region = icvar->FindVar("ce_region")->GetString();
     logReaderWaitTime = icvar->FindVar("ce_logreaderwaittime")->GetInt();
