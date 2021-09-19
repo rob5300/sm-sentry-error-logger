32fed976e4536f87289042717900e0f314146e87
Robert <rob5300@hotmail.com>
2021-09-19 16:47:30 +0100

    Add version to sln. Change version to 1.2

--- smsdk_config.h
+++ smsdk_config.h
@@ -40,7 +40,7 @@
 /* Basic information exposed publicly */
 #define SMEXT_CONF_NAME			"Sentry Error Logger"
 #define SMEXT_CONF_DESCRIPTION	"Capture SP exceptions and SM errors and report on the web via Sentry."
-#define SMEXT_CONF_VERSION		"1.0.0.1"
+#define SMEXT_CONF_VERSION		"1.2"
 #define SMEXT_CONF_AUTHOR		"rob5300 - Creators.TF"
 #define SMEXT_CONF_URL			"https://github.com/rob5300/sm-sentry-error-logger"
 #define SMEXT_CONF_LOGTAG		"Sentry Error Logger"
